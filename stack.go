f000d11edc59f99256497e6d613665d446b5031a
Christopher Lamm <chris@theantichris.com>
2020-11-17 07:06:54 -0500

    implemented the Stack

---
+++ stack.go
@@ -0,0 +1,79 @@
+package gostack
+
+import "sync"
+
+// Item holds the data that will be stored on the Stack.
+type Item interface{}
+
+// Stack represents the stack data structure.
+type Stack struct {
+	items []Item
+	mutex sync.Mutex
+}
+
+// Push adds a new Item to the Stack.
+func (stack *Stack) Push(item Item) {
+	stack.mutex.Lock()
+	defer stack.mutex.Unlock()
+
+	stack.items = append(stack.items, item)
+}
+
+// Pop removes and returns the top item from the Stack.
+func (stack *Stack) Pop() Item {
+	stack.mutex.Lock()
+	defer stack.mutex.Unlock()
+
+	if stack.IsEmpty() {
+		return nil
+	}
+
+	lastItem := stack.items[stack.Length()-1]
+	stack.items = stack.items[:stack.Length()-1]
+
+	return lastItem
+}
+
+// IsEmpty returns true if there are no Items on the Stack.
+func (stack *Stack) IsEmpty() bool {
+	stack.mutex.Lock()
+	defer stack.mutex.Unlock()
+
+	return stack.Length() == 0
+}
+
+// Reset removes all Items from the Stack.
+func (stack *Stack) Reset() {
+	stack.mutex.Lock()
+	defer stack.mutex.Unlock()
+
+	stack.items = nil
+}
+
+// Dump returns all the Items in the stack.
+func (stack *Stack) Dump() []Item {
+	stack.mutex.Lock()
+	defer stack.mutex.Unlock()
+
+	copied := make([]Item, stack.Length())
+	copy(copied, stack.items)
+
+	return copied
+}
+
+// Peek returns the top Item in the stack.
+func (stack *Stack) Peek() Item {
+	stack.mutex.Lock()
+	defer stack.mutex.Unlock()
+
+	if stack.IsEmpty() {
+		return nil
+	}
+
+	return stack.items[stack.Length()-1]
+}
+
+// Length returns the number of Items on the Stack.
+func (stack *Stack) Length() int {
+	return len(stack.items)
+}
